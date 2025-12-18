7db80b495bcd8184ac9b8aa912a587661bafd6a2
지민 <jimin@jimin-ui-MacBookAir.local>
2025-12-19 00:40:59 +0900

    llllll

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,10 +1,10 @@
 import { http, createConfig } from 'wagmi'
 import { sepolia } from 'wagmi/chains'
-import { SEPOLIA_RPC_URL } from './constants'
+import { SEPOLIA_RPC_URLS } from './constants'
 
 export const config = createConfig({
-    chains: [sepolia],
-    transports: {
-        [sepolia.id]: http(SEPOLIA_RPC_URL),
-    },
+  chains: [sepolia],
+  transports: {
+    [sepolia.id]: http(SEPOLIA_RPC_URLS[0]),
+  },
 })
